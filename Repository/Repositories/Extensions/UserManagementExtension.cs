c64df3f84070440637ee9d16834b772b8eba20ac
tinnguyentrung962k <trungtin9620@gmail.com>
2024-05-30 01:46:56 +0700

    rename folder to extensions

--- Repository/Repositories/Extensions/UserManagementExtension.cs
+++ Repository/Repositories/Extensions/UserManagementExtension.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repository.Repositories.Extension
+namespace Repository.Repositories.Extensions
 {
     public static class UserManagementExtension
     {
