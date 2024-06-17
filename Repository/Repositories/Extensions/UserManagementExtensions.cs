6706967130f76976f9b1209ee4a4d145bd563b06
tinnguyentrung962k <trungtin9620@gmail.com>
2024-06-18 00:52:34 +0700

    get user list with full profile dto

--- Repository/Repositories/Extensions/UserManagementExtensions.cs
+++ Repository/Repositories/Extensions/UserManagementExtensions.cs
@@ -1,6 +1,8 @@
-﻿using Domain.Entities;
+﻿using CrossCutting.Constants;
+using Domain.Entities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata;
 
 namespace Repository.Repositories.Extensions
 {
@@ -8,6 +10,14 @@ namespace Repository.Repositories.Extensions
     {
         public static async Task<User>? FindRefreshTokenAsync(this UserManager<User> userManager, string refreshToken) {
             return await userManager?.Users?.FirstOrDefaultAsync(u => u.RefreshToken.Equals(refreshToken));
+        }
+        public static async Task<IEnumerable<User>> GetUsersAsync(this UserManager<User> userManager, int pageIndex = 1 , int pageSize = 1, string roleName = RoleConstants.USER)
+        {
+            var userList = await userManager?.GetUsersInRoleAsync(roleName);
+            pageIndex = pageIndex < 1 ? 0 : pageIndex - 1;
+            pageSize = pageSize < 1 ? 10 : pageSize;
+            var pagedUsers = userList.Skip(pageIndex * pageSize).Take(pageSize);
+            return pagedUsers;
         }
     }
 }
---
+++ StartedIn/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using AutoMapper;
+using CrossCutting.DTOs.ResponseDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Services.Interface;
+using Services.Exceptions;
+
+namespace StartedIn.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IMapper _mapper;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(IUserService userService, IMapper mapper, ILogger<UserController> logger)
+        {
+            _mapper = mapper;
+            _userService = userService;
+            _logger = logger;
+        }
+        [HttpGet("users")]
+        public async Task<ActionResult<IEnumerable<FullProfileDTO>>> GetUserLists([FromQuery] int pageIndex, int pageSize)
+        {
+            try
+            {
+                var userList = await _userService.GetUsersList(pageIndex, pageSize);
+                var responseUserList = _mapper.Map<List<FullProfileDTO>>(userList);
+                return responseUserList;
+            }
+            catch (NotFoundException ex)
+            {
+                _logger.LogError(ex, "No user found.");
+                return NotFound(ex);
+            }
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, "Error while getting users list.");
+                return StatusCode(500,"Lỗi server");
+            }
+
+        }
+    }
+}
