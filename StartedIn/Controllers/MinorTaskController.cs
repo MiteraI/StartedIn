2a1b902e1cee08c0571335f7933348128d8b3879
tinnguyentrung962k <trungtin9620@gmail.com>
2024-07-18 13:36:55 +0700

    refactor controller for tasks

---
+++ StartedIn/Controllers/MinorTaskController.cs
@@ -0,0 +1,78 @@
+﻿using AutoMapper;
+using CrossCutting.DTOs.RequestDTO;
+using CrossCutting.DTOs.ResponseDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Services.Interface;
+using Services.Exceptions;
+
+namespace StartedIn.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class MinorTaskController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ILogger<MinorTaskController> _logger;
+        private readonly IMinorTaskService _minorTaskService;
+
+        public MinorTaskController(IMapper mapper, ILogger<MinorTaskController> logger, IMinorTaskService minorTaskService)
+        {
+            _minorTaskService = minorTaskService;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        [HttpPost("minortask/create")]
+        public async Task<ActionResult<MinorTaskResponseDTO>> CreateNewMinorTask(MinorTaskCreateDTO minorTaskCreateDto)
+        {
+            try
+            {
+                var minorTask = await _minorTaskService.CreateMinorTask(minorTaskCreateDto);
+                var response = _mapper.Map<MinorTaskResponseDTO>(minorTask);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while creating new minor task.");
+                return StatusCode(500, "Lỗi server");
+            }
+        }
+
+        [HttpPut("minortask/move")]
+        public async Task<ActionResult<MinorTaskResponseDTO>> MoveMinorTask(UpdateMinorTaskPositionDTO updateMinorTaskPositionDTO)
+        {
+            try
+            {
+                var responseMinorTask = _mapper.Map<MinorTaskResponseDTO>(await _minorTaskService.MoveMinorTask(updateMinorTaskPositionDTO.Id, updateMinorTaskPositionDTO.TaskboardId, updateMinorTaskPositionDTO.Position, updateMinorTaskPositionDTO.NeedsReposition));
+                return Ok(responseMinorTask);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Di chuyển Task nhỏ thất bại");
+            }
+        }
+
+        [HttpPut("minortask/edit/{id}")]
+        public async Task<ActionResult<MinorTaskResponseDTO>> EditInfoMinorTask(string id, [FromBody] UpdateMinorTaskInfoDTO updateMinorTaskInfoDTO)
+        {
+            try
+            {
+                var responseMinorTask = _mapper.Map<MinorTaskResponseDTO>(await _minorTaskService.UpdateMinorTaskInfo(id, updateMinorTaskInfoDTO));
+                return Ok(responseMinorTask);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Cập nhật thất bại");
+            }
+        }
+    }
+}
---
+++ StartedIn/Controllers/TaskBoardController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using CrossCutting.DTOs.RequestDTO;
+using CrossCutting.DTOs.ResponseDTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service.Services.Interface;
+using Services.Exceptions;
+
+namespace StartedIn.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class TaskBoardController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ILogger<TaskBoardController> _logger;
+        private readonly ITaskboardService _taskboardService;
+
+        public TaskBoardController(IMapper mapper, ILogger<TaskBoardController> logger, ITaskboardService taskboardService)
+        {
+            _taskboardService = taskboardService;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        [HttpPost("taskboard/create")]
+        public async Task<ActionResult<TaskboardResponseDTO>> CreateNewTaskboard(TaskboardCreateDTO taskboardCreateDto)
+        {
+            try
+            {
+                var taskboard = await _taskboardService.CreateNewTaskboard(taskboardCreateDto);
+                var response = _mapper.Map<TaskboardResponseDTO>(taskboard);
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error while creating new taskboard.");
+                return StatusCode(500, "Lỗi server");
+            }
+        }
+
+        [HttpPut("taskboard/move")]
+        public async Task<ActionResult<TaskboardResponseDTO>> MoveTaskBoard(UpdateTaskBoardPositionDTO updatetaskBoardPositionDTO)
+        {
+            try
+            {
+                var responseTaskBoard = _mapper.Map<TaskboardResponseDTO>(await _taskboardService.MoveTaskBoard(updatetaskBoardPositionDTO.Id, updatetaskBoardPositionDTO.Position, updatetaskBoardPositionDTO.NeedsReposition));
+                return Ok(responseTaskBoard);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Di chuyển bảng làm việc thất bại");
+            }
+        }
+
+        [HttpGet("taskboard/{id}")]
+        public async Task<ActionResult<TaskboardResponseDTO>> GetTaskboardById([FromRoute] string id)
+        {
+            try
+            {
+                var response = _mapper.Map<TaskboardResponseDTO>(await _taskboardService.GetTaskboardById(id));
+                return Ok(response);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Lỗi Server");
+            }
+
+
+        }
+    }
+}
